2fa2702375a59bf7d43df45889dd6bf6462ac90f
TomyHertzz <tomas.hertz.@gmail.com>
2025-12-22 03:46:26 -0300

    Just a commiting the learning

---
+++ FirstSteps/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Needed in order to read or write standard input/output soooo imma do a simple "Hello world program and grow from there"
+
+int main() {
+  std::cout << "Hello World!\n";
+  return 0;
+}
